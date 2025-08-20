4bba96321819196c9c2ed31f8f58c46401262830
njw235 <njw235@gmail.com>
2025-08-20 14:54:53 -0400

    Added more context to outputs

--- julia/SmallExample.jl
+++ julia/SmallExample.jl
@@ -1,6 +1,7 @@
 include("momentLS.jl")
 R"library(momentLS)"
 # Initialize a moment sequence supported by a single atom
+
 Random.seed!(1234)
 atom = rand(Uniform(0,.9))
 weight = rand(Uniform(0,3))
@@ -17,12 +18,15 @@ m = momentLSmod(y, dhat, [0.0],[0.0], 1e-8)
 
 supp = m[1]
 w= m[2]
-
+pop!(supp)
+pop!(w)
 @rput supp
 @rput w
 
 R"diff = L2diff_L2Moment(y, supp, w)"
 
 @rget diff
-print(supp, w)
-print(diff)
+
+print("The estimated support is: ", supp, " The estimated weights are: ", w)
+print("The true support is: ", atom, " The true weight is: ", weight)
+print("The loss function value for our estimator is: ", diff)
