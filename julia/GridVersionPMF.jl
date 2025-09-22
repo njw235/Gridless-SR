e81f66e67815e229c6bd8b0a22b1d92dba0c44f0
njw235 <njw235@gmail.com>
2025-09-22 11:32:10 -0400

    Update GridVersionPMF.jl

--- julia/GridVersionPMF.jl
+++ julia/GridVersionPMF.jl
@@ -244,7 +244,10 @@ end
 	# Two finite mixtures of geometrics and a mixture model with
 	# Uniform distribution as the mixing measure 
 	
-	
+
+function pmft3(x)
+	1/((x+1)*(x+2))
+end
 function pmft2(x)
 	    0.25*0.9*0.1^x + 0.5*0.7*0.3^x + 0.25*0.2*0.8^x
 end
@@ -252,22 +255,19 @@ end
 function pmft1(x)
 	    (1/3)*0.8*0.2^x + (2/3)*0.6*0.4^x
 end
-errordict = Dict()
-	
+
+i = ARGS[1]	
 Random.seed!(1234);
-	
-for N in [50,100,500,1000,5000,10000]
-	errors = zeros(2,20)
-	werrors = zeros(2,20)
-	for j in 1:20
-	        for i in 1:2
-	
+errors = zeros(100)
+	for j in 1:100
 	            p = sim_data(N,i)
 	
 	            if(i == 1)
 	                d = 0.4
 	            elseif(i == 2)
 	                d = 0.1
+				else
+					d = 0.002
 	            end
 	
 	            supp = [0.1]
@@ -286,24 +286,17 @@ for N in [50,100,500,1000,5000,10000]
 	            x = [0:1:10000;]
 	
 	            if(i == 1)
-	                errors[i,j] = sum((pmf.(x) .- pmft1.(x)).^2)
+	                errors[j] = sum((pmf.(x) .- pmft1.(x)).^2)
 	               
 	            elseif(i == 2)
-	                errors[i,j] = sum((pmf.(x) .- pmft2.(x)).^2)
-	        end
+	                errors[j] = sum((pmf.(x) .- pmft2.(x)).^2)
+				else
+					errors[j] = sum((pmf.(x) .- pmft3.(x)).^2)
+				end
 	
 	    end
-	
-	    errorlist = reduce(+, eachcol(errors)) ./ size(errors,2)
-	
-	    
-	    errordict[N] = errorlist 
-	
-	end
-end	
 
-	print(errordict)
 
-open("gridpmf.txt", "w") do io
+open(string("gridpmf", ARGS[1], ".txt"), "w") do io
   print(io, errordict)
 end
