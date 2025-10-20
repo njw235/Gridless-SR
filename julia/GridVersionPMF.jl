56d3dd41f09f174578ec0bcaa45a30829eef93c3
njw235 <njw235@gmail.com>
2025-10-20 08:06:13 -0400

    Update GridVersionPMF.jl

--- julia/GridVersionPMF.jl
+++ julia/GridVersionPMF.jl
@@ -5,197 +5,8 @@ using RCall
 #Adding in the grid based implementation of the support reduction algorithm
 # Documentation and code can be found at https://people.math.ethz.ch/~fadouab/ComparisonEstimSR.r
 
-R"interval = seq(0,1,length = 501)"
-R"Emp <- function(x) {  
-   # compute the empirical frequence of x
-   n = length(x)
-   supp <- seq(0,max(x),by=1)
-   count <- rep(0,length(supp))
-   for(i in 1:length(count)) {
-     count[i] <- sum(x==supp[i])
-   }
-   
-   return(list(supp=supp,freq=count/n))
-   
-   
- }"
-R"InitializeC = function(x, alpha){
-  V  = 0:max(x)
-  T = sum(Emp(x)$freq*alpha^V)
-  
-  return((1+alpha) * T)
-  
-}
-"
-R"
-Interm = function(x,alpha){
-  V=0:max(x)
-  return(sum(Emp(x)$freq*alpha^V))
-}"
-R"DirDeriv = function(alpha, x, S, C){
-  # C is the vector of weights  of the current iterate
-  # S is the support vector of the current iterate
-  
-  T1 = sum(C* (1-S)/(1-S*alpha))
-  T2 = Interm(x=x, alpha=alpha)
-  
-  return((1-alpha)* (T1 - T2))
-  
-}"
-R"FindMinfun = function(x, S, C){
-  
-  V = 0:max(x)
-  Out1 = rep(0, length(interval))
-  Out2 = rep(0, length(interval))
-  
-  for(j in 1:length(S)){
-    
-    Out1 = Out1 + C[j]*(1-S[j])/(1-S[j]*interval)
-  }
-  
-  
-  for(j in 1:length(V)){
-    
-    Out2 = Out2 + Emp(x)$freq[j]*interval^{V[j]}
-  }
-  
-  Out = (1-interval)*(Out1 - Out2)
-  Ind =match(min(Out), Out)
-  
-  return(list(minimum=interval[Ind],objective=min(Out)))
-  
-}"
-R"SolveUncons  = function(x, S){
-  
-  M = matrix(0, length(S), length(S))
-  for(i in 1:length(S)){
-    
-    for(j in 1:length(S)){
-      M[i,j]  = (1-S[j])/(1-S[i]*S[j])
-    }
-    
-  }
-  #M1=rep(1,length(S))%*%t(1-S)
-  #M2 = 1-S%*%t(S)
-  #M =M1/M2
-  
-  B= rep(0, length(S))
-  for(i in 1:length(S)){
-    B[i]  = Interm(x=x, alpha=S[i])
-  }
-  Sol = solve(M, B)
-  Sol= as.vector(Sol)
-  List = list(Sol = Sol, Check1=as.vector(M%*%Sol)-B)
-  
-  return(List$Sol)
-}"
+R"library(momentLS)"
 
-R"ReduceSuppfun = function(S1, C1, S2, C2){
-  
-  S.m = c(S1, S2)
-  S.m = unique(sort(S.m))
-  
-  # S1 will always be the support vector of the previous permissible iterate
-  #  S2 will be the current support vector. An example:
-  
-  # S1=  c(0.2, 0.3, 0.4)
-  # S2 = c(0.1, 0.2, 0.3, 0.4)  (this means that the point 0.1 was added to S1
-  # Then S.m = c(0.1, 0.2, 0.3, 0.4) 
-  
-  S2.m = rep(10, length(S.m)) 
-  
-  # S2.m is a vector filled with the value 10. This choice is really arbitrary.
-  
-  C1.m = rep(0, length(S.m))  # we will store weights in this vector
-  
-  C2.m = rep(0, length(S.m))  # idem
-  
-  ind1 = pmatch(S1, S.m)
-  ind2 = pmatch(S2, S.m)
-  
-  # The vector ind1 identifies the indices where S.m has the same values as in S1
-  # The same thing for ind1
-  # In the example given above we have: ind1 = c(2,3,4) and ind2 = c(1,2,3,4)
-  # 
-  
-  
-  
-  C1.m[ind1]  = C1   # we fill the right positions with the weights from C1
-  C2.m[ind2]  = C2   # idem
-  
-  S2.m[ind2]  = S2    # we fill the right positions with the support points from S2
-  
-  Lambda = C1.m/(C1.m-C2.m)  # we compute this ratio (componentwise) as the mimimal value of the positive
-  # elements of Lambda gives the right convex combination (which will delete
-  # one of the support point of S2
-  
-  index = match(min(Lambda[Lambda > 0]), Lambda)  # We look for the position of the minimum
-  
-  S2new  = S2.m[-index]   # We remove that support points from the augmented support vector corresponding to S2
-  S2new = S2new[S2new < 10]  # We get rid of the values 10 to get finally the reduced suppport vector 
-  
-  return(S2new)
-}"
-R"CompMonLSE= function(x,alpha0,epsilon){
-  
-  S0=alpha0   # any intial value of choice
-  
-  C0 = InitializeC(x=x, alpha=alpha0)  # the initial optimal weight
-  #print(C0)
-  
-  Resmin = FindMinfun(x=x, S=S0,C=C0)  # Looking for the optimal Dirac direction in which we will go 
-  thetamin=Resmin$minimum              # This is the new support point that will be added
-  valmin = Resmin$objective
-  
-  count =0
-  Count = 0
-  while(abs(valmin) > epsilon ){   # a stopping condition
-    count <- count + 1	 
-    Snew=c(S0,thetamin)  # The new suport
-    Snew = sort(Snew)
-    
-    
-    Cnew=SolveUncons(x=x,S=Snew)  # Solve the unconstrained LS problem
-    
-    min.C <- min(Cnew) 
-    #Count <- 0
-    while(min.C < 0){  # condition indicating that the unconstrained solution is not permissible
-      # and that we should go into the reduction step
-      print(Count)
-      Count <- Count+1
-
-      Snew = ReduceSuppfun(S1=S0, C1=C0, S2=Snew, C2=Cnew)
-      
-      if(length(Snew) > 1){  # two expressions for the solutions depending on whether we are left with 
-        # more than 1 point or just one
-        Cnew = SolveUncons(x=x, S=Snew)
-      }
-      else{
-        Cnew=InitializeC(x=x, alpha=as.numeric(Snew))
-        
-      }  #  the program will go out of the inner loop (reduction loop) if 
-      # and only if the solution is permissible
-      
-      min.C = min(Cnew)
-      
-      
-    }#min.C
-    
-    S0= Snew  # uppdate the support vector
-    C0= Cnew  # updfate the weight vector
-    
-    Resmin = FindMinfun(x=x, S=S0,C=C0)  # compute the new minimizer of the directional derivative
-    # to build up a new support or to stop if the stopping 
-    #crierion is met. 
-    
-    thetamin=Resmin$minimum
-    valmin = Resmin$objective
-    #print(valmin)
-  }#valmin
-  
-  return(list(S0=S0,C0=C0, valmin=valmin))
-  
-}"
 
 
 
@@ -248,10 +59,10 @@ function pmft1(x)
   (1 / 3) * 0.8 * 0.2^x + (2 / 3) * 0.6 * 0.4^x
 end
 
-i = 3
+i = 1
 Random.seed!(1234);
-errors = zeros(50)
-for j in 1:50
+errors = zeros(100)
+for j in 1:100
   p = sim_data(1000, i)
 
   if (i == 1)
@@ -265,10 +76,25 @@ for j in 1:50
   supp = [0.1]
   weight = [0.5]
   @rput p
+  R"counts=table(p)"
+  R"pBar=rep(0,max(p)+1)"
+  R"pBar[as.numeric(names(counts))+1]=counts/sum(counts)"
+  R"alphaGrid=momentLS::makeGrid(upper_threshold = 1-1e-4,nX=201,cm=TRUE,scale='equidist')"
+  R"XtX=(1-alphaGrid)/(1-outer(alphaGrid,alphaGrid)) #use 1/(1-xy) inner product instead of (1+xy)/(1-xy)"
+  R"s_alpha=sqrt(diag(XtX))"
+  R"Xtr_0=Xtr_cpp(alphaGrid,pBar,standardization=FALSE,epsilon = 1e-10)"
+  R"Xtr=pBar[1]/2+Xtr_0/2"
+  R"XtX_standardized=diag(1/s_alpha)%*%XtX%*%diag(1/s_alpha)"
+  R"Xtr_standardized=Xtr/s_alpha"
   R"m = CompMonLSE(p, 0.1, 1e-8)"
-
-  R"supp = m$S0"
-  R"weight = m$C0"
+  R"fit=SR1(r=pBar,delta=NULL,alphaGrid = alphaGrid,
+        precomputed=list(s_alpha=s_alpha,
+                         XtX=XtX_standardized,
+                         Xtr=Xtr_standardized,
+                         alphaGrid=alphaGrid,
+                         input=pBar))"
+  R"supp = fit$support"
+  R"weight = fit$weights/ (1-fit$support)"
   @rget supp
   @rget weight
   function pmf(x)
