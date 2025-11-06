c36dd98ca5d9354ed20578ea0889fa86d89d50ab
njw235 <njw235@gmail.com>
2025-11-06 14:40:49 -0500

    Integrate RCall and update simulation parameters

--- src/pmfestimationex.jl
+++ src/pmfestimationex.jl
@@ -1,5 +1,8 @@
 include("mixingMeasure.jl")
-
+using RCall
+R"library(momentLS)"
+Random.seed!(1234);
+alphas = rand(10)
 function sim_data(n, option)::AbstractArray{Integer}
     data = []
     for i in 1:n
@@ -21,9 +24,16 @@ function sim_data(n, option)::AbstractArray{Integer}
                 append!(data, rand(Geometric(0.2)))
             end
         
-        else
+        elseif(option == 3)
             r = rand(Uniform(0,1))
             append!(data, rand(Geometric(1-r)))
+        else
+            r = rand(Uniform(0,1))
+            for j in 1:10
+                if r > (j-1)/10 && r < j/10
+                    append!(data, rand(Geometric(alphas[j])))
+                end
+            end
         end
     end
     return(data)
@@ -36,7 +46,11 @@ end
 # Using the two pmfs from the paper given
 # Two finite mixtures of geometrics
 
-i = ARGS[1]
+i = parse(Int64, ARGS[1])
+
+function pmft4(x)
+    sum(0.1 .* alphas .* (1 .- alphas).^x)
+end
 function pmft3(x)
     1/((x+1)*(x+2))
 end
@@ -48,46 +62,76 @@ end
 function pmft1(x)
     (1/3)*0.8*0.2^x + (2/3)*0.6*0.4^x
 end
-errordict = Dict()
 
-Random.seed!(1234);
+R"momentNorm = function(support, weights) {
+aat = outer(support, support)
+norm2 = sum(1/(1-aat) * outer(weights, weights))
+return(norm2)
+}"
+
+
+R"innerProduct = function(s1, w1, s2, w2){
+aat = outer(s1, s2)
+ip = sum(1/(1-aat) * outer(w1,w2))
+return(ip)
+}"
+
+R"twomomentdiff = function(s1, w1, s2,w2){
+val = momentNorm(s1, w1) + momentNorm(s2,w2) - 2*innerProduct(s1,w1,s2,w2)
+return(val)
+}"
+
+
+
+
 
 N = 1000
 errors = zeros(50)
+oerrors = zeros(50)
 for j in 1:50
     p = sim_data(N,i)
     r = Empirical(p)
     if(i == 1)
-        d = 0.4
+        d = 0.01
     elseif(i == 2)
-        d = 0.1
-    else
+        d = 0.01
+    elseif(i==3)
         d = 0.001
+    else
+        d = 1- findmax(alphas)[1]
     end
     supp = [0.1]
     weight = [0.5]
-    m = mixingmeasure(r, d, supp, weight, 1e-8)
+    m = mixingmeasure(r, d, supp, weight, 3e-8)
     supp = m[1]
     weight = m[2]
     function pmf(x)
             sum(weight .* supp.^x)
     end        
-     x = [0:1:10000;]
+     x = [0:1:10000000;]
     if(i == 1)
             errors[j] = sum((pmf.(x) .- pmft1.(x)).^2)               
     elseif(i == 2)
             errors[j] = sum((pmf.(x) .- pmft2.(x)).^2)               
+    elseif(i==3)
+            errors[j] = sum((pmf.(x) .- pmft3.(x)).^2)   
     else
-            errors[j] = sum((pmf.(x) .- pmft3.(x)).^2)                
+            errors[j] = sum((pmf.(x) .- pmft4.(x)).^2) 
     end
+    @rput supp
+    @rput weight
+    @rput r
+    R"err = L2diff_L2Moment(r, supp, weight)"
+    @rget err
+    oerrors[j] = err
+    println(errors[j], oerrors[j])
 end
 
 
 
-end
 
 
-
-open(string("gridlesspmf",ARGS[1], ".txt", "w") do io
-    print(io, errors)
+open(string("gridlesspmf",ARGS[1], ".txt"), "w") do io
+    println(io, errors)
+    println(io, oerrors)
 end
