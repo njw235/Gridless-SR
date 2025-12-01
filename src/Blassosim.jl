aa891275e0f04f62d169095c121aa9fae591bd12
njw235 <njw235@gmail.com>
2025-12-01 11:45:42 -0500

    Enhance Blassosim.jl with measurement storage
    Added measurement list and saving functionality to R.

--- src/Blassosim.jl
+++ src/Blassosim.jl
@@ -3,6 +3,7 @@ R"load('MC_chains.Rdata')"
 R"library(momentLS)"
 R"print(getwd())"
 R"source('simulateBLasso.R')"
+R"measlist = vector('list', length = 50)"
 i = ARGS[1]
 errors = zeros(50)
 for j in 1:50
@@ -23,7 +24,10 @@ for j in 1:50
     @rget err
     errors[j] = err
     println(err)
+    R"measlist[[j]] = cbind(supp, weight)"
 end
+R"print(measlist)"
+R"saveRDS(measlist, paste(i, 'Blasso.rds', sep = ''))"
 open(string("BLasso", i, ".txt"), "w") do io
     println(io, errors)
 end
